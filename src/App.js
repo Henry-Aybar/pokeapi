32bf1916e35288bc8760011504e53d8c2f1b2e4f
Henry Aybar <aybar.henry.usmc@gmail.com>
2021-11-04 09:35:48 -0500

    807 pokemon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [resData, setResData] = useState([]);
+  const fetchPoke = ()=>{
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=807')
+      .then(res=>{ setResData(res.data.results) })
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={ fetchPoke } >Fetch Pokemon</button>
+      <div>
+            {resData.length > 0 && resData.map((pokemon, index)=>{
+                return (<div key={index}>{pokemon.name}</div>)
+            })}
+        </div>
     </div>
   );
 }
